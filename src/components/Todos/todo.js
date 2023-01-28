6d30572cb2527b4bd9a6811577c250dc4be0cb3b
Your NamePavelVslv <pavelexm@gmail.com>
2023-01-28 14:26:45 +0000

    change delete from double click to icon click

--- src/components/Todos/todo.js
+++ src/components/Todos/todo.js
@@ -1,16 +1,14 @@
-import { FaBeer , FaCheck , FaEdit } from 'react-icons/fa'
+import { FaBeer, FaCheck, FaEdit } from 'react-icons/fa'
 import styles from '../Todos/todo.module.css'
 
 function Todo(props) {
   const { key, todo, del } = props
   return (
-    <div className={styles.todo} onDoubleClick={() => del(todo.id)}>
+    <div className={styles.todo}>
       <FaBeer className={styles.todoIcon} />
       <div className={styles.todoText}>{todo.text}</div>
-      <FaEdit className={styles.deleteIcon} />
+      <FaEdit className={styles.deleteIcon} onClick={() => del(todo.id)} />
       <FaCheck className={styles.checkIcon} />
-
-
     </div>
   )
 }
